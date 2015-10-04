23174fd2da17ca36c51eda3007da88698e180b41
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-03 21:10:14 -0700

    working method (needs refactoring)

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,16 +4,25 @@ def caesar_cipher(str, num=0)
 	numbers = ("0".."9").to_a
 	numAlpha = num % 26
 	numNumer = num % 10
-	i,j,k=0,0,0
-	strArray = str.split(//)
-	strArray.each do |char|
+	i = 0
+	str.each_char do |char|
 		if char =~ /[A-Z]/
-			if char = char.upcase
-				newIndex = (uppers.index(char) + numAlpha)%26
-				strArray[i] = uppers[newIndex]
-				i += 1
-			end
+			newIndex = (uppers.index(char) + numAlpha)%26
+			str[i] = uppers[newIndex]
+			i += 1
+		elsif char =~ /[a-z]/
+			newIndex = (lowers.index(char) + numAlpha)%26
+			str[i] = lowers[newIndex]
+			i += 1
+		elsif char =~ /[0-9]/
+			newIndex = (numbers.index(char) + numNumer)%10
+			str[i] = numbers[newIndex]
+			i += 1
+		else
+			i += 1
 		end
 	end
-	strArray
+	puts str
 end
+
+caesar_cipher("This is difficult to the power of 8675309!",5)
