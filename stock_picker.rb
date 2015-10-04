b871bda0c1f5732aa9981ecd850a10454257ccd4
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-04 00:45:38 -0700

    Method stock_picker up and running

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,26 @@
 def stock_picker(arr)
-	if arr.index(arr.min) < arr.index(arr.max)
-		arrIdeal = [arr.index(arr.min),arr.index(arr.max)]
+	diffArray = []
+	setArray = []
+	arr.each_with_index do |element1, index1|
+		arr.each_with_index do |element2, index2|
+			if index2 >= index1
+				difference = element2 - element1
+				diffArray << difference
+				setArray << [index1, index2]
+			end
+		end
 	end
-	puts arrIdeal
+	puts setArray[diffArray.index(diffArray.max)]
 end
+
+=begin
+	Each time I find a difference, I put the difference
+	in one array and the corresponding indeces in
+	another array simultaneously (this way, the difference
+	and the set of indeces have the same index). Then I
+	take the index of the maximum difference and use that
+	same index in the array of index sets and find the
+	corresponding indeces.
+=end
 
 stock_picker([2,4,3,5,1,7,9,3,5,6])
