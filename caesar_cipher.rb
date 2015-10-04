63d17b4a3b0d65c49016ca55c8bd4d911077dabb
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-03 18:59:47 -0700

    first attempt replacing characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,4 +2,16 @@ def caesar_cipher(str, num=0)
 	uppers = ("A".."Z").to_a
 	lowers = ("a".."z").to_a
 	numbers = ("0".."9").to_a
+	numAlpha = num % 26
+	numNumer = num % 10
+	i,j,k=0,0,0
+	strArray = str.split(//)
+	strArray.each do |char|
+		if char =~ /A-Z/
+			newIndex = (uppers.index(char) + numAlpha)%26
+			strArray[i] = uppers[newIndex]
+			i += 1
+		end
+	end
+	strArray
 end
