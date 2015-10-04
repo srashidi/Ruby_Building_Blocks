3559673406857bcda2df033b8a472e590febe72f
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-03 23:15:27 -0700

    Refactored and ready to go

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Ruby_Building_Blocks
+http://www.theodinproject.com/ruby-programming/building-blocks
--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,27 +2,34 @@ def caesar_cipher(str, num=0)
 	uppers = ("A".."Z").to_a
 	lowers = ("a".."z").to_a
 	numbers = ("0".."9").to_a
-	numAlpha = num % 26
-	numNumer = num % 10
-	i = 0
+	@i = 0
+	@str = str
+	@num = num
+
+=begin
+I refactored the shift method for each character.
+It can now take any set of letters, numbers, symbols, etc. from an array.
+All I need to do is enter the method, specifying the array.
+=end
+
+	def shift_action(arr, char)
+		newIndex = (arr.index(char) + @num%arr.length)%arr.length
+		@str[@i] = arr[newIndex]
+	end
+
 	str.each_char do |char|
 		if char =~ /[A-Z]/
-			newIndex = (uppers.index(char) + numAlpha)%26
-			str[i] = uppers[newIndex]
-			i += 1
+			shift_action(uppers, char)
 		elsif char =~ /[a-z]/
-			newIndex = (lowers.index(char) + numAlpha)%26
-			str[i] = lowers[newIndex]
-			i += 1
+			shift_action(lowers, char)
 		elsif char =~ /[0-9]/
-			newIndex = (numbers.index(char) + numNumer)%10
-			str[i] = numbers[newIndex]
-			i += 1
-		else
-			i += 1
+			shift_action(numbers, char)
 		end
+		@i += 1
 	end
-	puts str
+
+	puts @str
+
 end
 
-caesar_cipher("This is difficult to the power of 8675309!",5)
+caesar_cipher("This is EXTREMELY difficult to the power of 8675309!",5)
