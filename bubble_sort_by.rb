a6d9c524c85eddb28acb6fbc1793de85561e525a
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-07 11:42:18 -0700

    Method bubble_sort_by up and running

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,13 +1,25 @@
 def bubble_sort_by(arr,&block)
-	arr.each_with_index do |value, index|
+	until (arr.each_with_index do |value, index|
 		if index < (arr.size - 1)
-			if block.call(value,arr[index+1]) > 0
-				arr[index] = arr[index+1]
-				arr[index+1] = value
+			if block.call(value,arr[index+1]) <= 0
+				true
+			else
+				false
+				break
+			end
+		end
+	end)
+		arr.each_with_index do |value, index|
+			if index < (arr.size - 1)
+				if block.call(value,arr[index+1]) > 0
+					arr[index] = arr[index+1]
+					arr[index+1] = value
+				end
 			end
 		end
 	end
 	puts arr.inspect
 end
 
+bubble_sort_by(["hi","howdy","hello","hey","hiho"]) {|left, right| left.length - right.length}
 bubble_sort_by(["hi","hello","hey"]) {|left, right| left.length - right.length}
