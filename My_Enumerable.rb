323e468b55935731db7675ce553d2514177b1f30
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-07 15:03:11 -0700

    my_select, my_all?, and my_any? up and running

--- My_Enumerable.rb
+++ My_Enumerable.rb
@@ -18,9 +18,130 @@ module Enumerable
 		end
 	end
 
+	def my_select
+		if block_given?
+			result = self.class.new
+			if self.class == Array
+				i = 0
+				self.my_each do |element|
+					if yield(element)
+						result[i] = element
+						i += 1
+					end
+				end
+			elsif self.class == Hash
+				self.my_each do |key, value|
+					if yield(key,value)
+						result[key] = value
+					end
+				end
+			end
+			result
+		end
+	end
+
+	def my_all?
+		result = true
+		if block_given?
+			self.my_each do |element|
+				if yield(element)
+					result = true
+				else
+					result = false
+					break
+				end
+			end
+		else
+			self.my_each do |element|
+				if element
+					result = true
+				else
+					result = false
+					break
+				end
+			end
+		end
+		result
+	end
+
+	def my_any?
+		result = true
+		if block_given?
+			self.my_each do |element|
+				if yield(element)
+					result = true
+					break
+				else
+					result = false
+				end
+			end
+		else
+			self.my_each do |element|
+				if element
+					result = true
+					break
+				else
+					result = false
+				end
+			end
+		end
+		result
+	end
+
 end
+
+[1,2,3,4].each {|value| puts value*2}
+{:Shaunty => "Hossein", :Austin => "Danger"}.each {|key, value| puts value+"Blah"}
+
+[1,2,3,4].each_with_index {|value,index| print "#{value*2} #{index} "}
+{:Shaunty => "Hossein", :Austin => "Danger"}.each_with_index {|(key, value),index| puts value+"Blah #{index}"}
+
+puts [1,2,3,4].select {|value| value < 3}.inspect
+h1 = {Shaunty: "Hossein", Austin: "Danger"}.select {|key, value| value.include?("H")}
+puts h1.to_s
+
+puts [1,2,3,4].all? {|value| value%2 == 0}
+puts [1,2,3,4].all? {|value| value > 0}
+h2 = {:Shaunty => "Hossein", :Austin => "Danger"}.all? {|key, value| value.include?("H")}
+h3 = {:Shaunty => "Hossein", :Austin => "Danger"}.all? {|key, value| value.include?("e")}
+puts h2.to_s
+puts h3.to_s
+
+puts [1,2,3,4].any? {|value| value%2 == 0}
+puts [1,2,3,4].any? {|value| value > 0}
+puts [1,2,3,4].any? {|value| value < 0}
+h4 = {:Shaunty => "Hossein", :Austin => "Danger"}.any? {|key, value| value.include?("H")}
+h5 = {:Shaunty => "Hossein", :Austin => "Danger"}.any? {|key, value| value.include?("e")}
+h6 = {:Shaunty => "Hossein", :Austin => "Danger"}.any? {|key, value| value.include?("z")}
+puts h4.to_s
+puts h5.to_s
+puts h6.to_s
+
+puts ""
 
 [1,2,3,4].my_each {|value| puts value*2}
 {:Shaunty => "Hossein", :Austin => "Danger"}.my_each {|key, value| puts value+"Blah"}
+
 [1,2,3,4].my_each_with_index {|value,index| print "#{value*2} #{index} "}
 {:Shaunty => "Hossein", :Austin => "Danger"}.my_each_with_index {|(key, value),index| puts value+"Blah #{index}"}
+
+puts [1,2,3,4].my_select {|value| value < 3}.inspect
+h1 = {Shaunty: "Hossein", Austin: "Danger"}.my_select {|key, value| value.include?("H")}
+puts h1.to_s
+
+puts [1,2,3,4].my_all? {|value| value%2 == 0}
+puts [1,2,3,4].my_all? {|value| value > 0}
+h2 = {:Shaunty => "Hossein", :Austin => "Danger"}.my_all? {|key, value| value.include?("H")}
+h3 = {:Shaunty => "Hossein", :Austin => "Danger"}.my_all? {|key, value| value.include?("e")}
+puts h2.to_s
+puts h3.to_s
+
+puts [1,2,3,4].my_any? {|value| value%2 == 0}
+puts [1,2,3,4].my_any? {|value| value > 0}
+puts [1,2,3,4].my_any? {|value| value < 0}
+h4 = {:Shaunty => "Hossein", :Austin => "Danger"}.my_any? {|key, value| value.include?("H")}
+h5 = {:Shaunty => "Hossein", :Austin => "Danger"}.my_any? {|key, value| value.include?("e")}
+h6 = {:Shaunty => "Hossein", :Austin => "Danger"}.my_any? {|key, value| value.include?("z")}
+puts h4.to_s
+puts h5.to_s
+puts h6.to_s
