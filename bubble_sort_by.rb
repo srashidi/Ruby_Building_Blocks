90b542a7fe9901a077afe1fee962b90799778784
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-07 11:06:09 -0700

    Method bubble_sort_by first iteration successful

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,2 +1,13 @@
-def bubble_sort_by(arr)
+def bubble_sort_by(arr,&block)
+	arr.each_with_index do |value, index|
+		if index < (arr.size - 1)
+			if block.call(value,arr[index+1]) > 0
+				arr[index] = arr[index+1]
+				arr[index+1] = value
+			end
+		end
+	end
+	puts arr.inspect
 end
+
+bubble_sort_by(["hi","hello","hey"]) {|left, right| left.length - right.length}
